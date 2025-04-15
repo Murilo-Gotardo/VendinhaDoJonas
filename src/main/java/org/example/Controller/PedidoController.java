6a4885fe2b808e5ee7c68d1dccf6959290f6ec16
Junior Osnildo <juniorosnildo777@gmail.com>
2025-04-15 09:27:20 -0300

    Adiciona a classe Pedidos e PedidosController muda tudo de lugar

---
+++ src/main/java/org/example/Controller/PedidoController.java
@@ -0,0 +1,17 @@
+﻿package org.example.Controller;
+
+import org.example.Models.Pedido;
+import org.example.Models.Produto;
+
+import java.util.List;
+
+public class PedidoController {
+    public void adicionarAoCarrinho(Produto produto, Pedido pedido){
+        double novoValor = pedido.getValorTotal() + produto.getValor();
+
+        pedido.getCarrinho().add(produto);
+        pedido.setValorTotal(novoValor);
+    }
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.Controller.ProdutoController;
+import org.example.Models.Produto;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         List<Produto> produtos = new ArrayList<Produto>();
-        ProdutoManager manager = new ProdutoManager(produtos);
+        ProdutoController manager = new ProdutoController(produtos);
 
         produtos.add(new Produto("pao", 20));
         produtos.add(new Produto("bolacha", 3));
